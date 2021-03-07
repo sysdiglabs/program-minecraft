e3d9df5e7b33ef95396c6e72d0be052dfd79b534
Angel Alvarado <angel.angelio@gmail.com>
2021-03-06 19:35:31 -0800

    config

--- vscode/src/hello.py
+++ vscode/src/hello.py
@@ -6,12 +6,15 @@ import time
 #https://github.com/martinohanlon/minecraft-stuff
 # https://github.com/martinohanlon/mcpi
 mc = Minecraft.create("minecraft-server")
-
 mc.postToChat("Hello World!")
-#mc.player.setTilePos(2, 0, 0)
+mc.player.setTilePos(2, 2, 1)
 melon = 100
+lana = 35
+hola = 125
+sign = 68
 #https://minecraft-ids.grahamedgecombe.com/
-for x in range(100):
+for y in range(50):
   time.sleep(0.5)
-  y = x+1
-  mc.setBlock(10, x, y, melon)
+  z = y + 1
+  mc.setBlock(10, y, z,lana)
+  mc.setSign(10, y, z,sign, 2)
